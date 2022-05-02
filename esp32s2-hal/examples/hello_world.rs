ee37a101898a86649a188389551c6d2b416fa1b4
Scott Mabin <scott@mabez.dev>
2022-05-02 14:29:04 +0200

    Example for wsr emulation

--- esp32s2-hal/examples/hello_world.rs
+++ esp32s2-hal/examples/hello_world.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
+#![feature(asm_experimental_arch)]
 
 use core::fmt::Write;
 
 use esp32s2_hal::{pac::Peripherals, prelude::*, RtcCntl, Serial, Timer};
 use nb::block;
 use panic_halt as _;
+use xtensa_atomic_emulation_trap as _;
 use xtensa_lx_rt::entry;
 
 #[entry]
@@ -22,8 +24,14 @@ fn main() -> ! {
 
     timer0.start(40_000_000u64);
 
+    xtensa_atomic_emulation_trap::test_print();
+
     loop {
         writeln!(serial0, "Hello world!").unwrap();
         block!(timer0.wait()).unwrap();
+
+        writeln!(serial0, "About to trigger exception").unwrap();
+        let x = 4;
+        unsafe { core::arch::asm!("wsr {}, SCOMPARE1", in(reg) x) };
     }
 }
--- esp32s2-hal/ld/rom.x
+++ esp32s2-hal/ld/rom.x
@@ -2,3 +2,6 @@ REGION_ALIAS("ROTEXT", irom_seg);
 REGION_ALIAS("RWTEXT", iram_seg);
 REGION_ALIAS("RODATA", drom_seg);
 REGION_ALIAS("RWDATA", dram_seg);
+
+
+PROVIDE ( uart_tx_one_char = 0x40012b10 );
